b86710ff86afe3b44aa1b50d43a891136b1f4897
shaunsebastian <shaunsebastian4@gmail.com>
2025-10-03 17:32:02 +0530

    ports config fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true, // binds to 0.0.0.0
+    allowedHosts: ['tb-front-40xi.onrender.com']
+  }
 })
